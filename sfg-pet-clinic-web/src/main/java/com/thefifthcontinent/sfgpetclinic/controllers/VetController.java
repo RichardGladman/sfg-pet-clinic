c6744aa7b06eb7a026901f235d763bf4cb233444
Richard Gladman <gitlab@thefifthcontinent.com>
2020-09-03 13:54:10 +0100

    Show list of vets

--- sfg-pet-clinic-web/src/main/java/com/thefifthcontinent/sfgpetclinic/controllers/VetController.java
+++ sfg-pet-clinic-web/src/main/java/com/thefifthcontinent/sfgpetclinic/controllers/VetController.java
@@ -1,14 +1,28 @@
 package com.thefifthcontinent.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.thefifthcontinent.sfgpetclinic.services.VetService;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 	
+	private final VetService vetService;
+	
+		
+	public VetController(VetService vetService) {
+		super();
+		this.vetService = vetService;
+	}
+
+
+
 	@RequestMapping({"", "/", "/index", "/index.html"})
-	public String listVets() {
+	public String listVets(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
 
