15687a4776cdde7388d780242a7010ab386677d5
Richard Gladman <gitlab@thefifthcontinent.com>
2020-09-05 14:24:45 +0100

    Add contact info to owner

--- sfg-pet-clinic-data/src/main/java/com/thefifthcontinent/sfgpetclinic/model/Owner.java
+++ sfg-pet-clinic-data/src/main/java/com/thefifthcontinent/sfgpetclinic/model/Owner.java
@@ -6,9 +6,43 @@ public class Owner extends Person {
 
 	private static final long serialVersionUID = 1L;
 	
+	private String address;
+	private String city;
+	private String phoneNumber;
+	
 	private Set<Pet> pets;
 
 	
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public String getCity() {
+		return city;
+	}
+
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+
 	public Set<Pet> getPets() {
 		return pets;
 	}
