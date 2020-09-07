f1bce23454d59c313f35842e76b5d9f414530d1f
Richard Gladman <gitlab@thefifthcontinent.com>
2020-09-07 12:25:53 +0100

    Pets and visits changed to JPA entities

--- sfg-pet-clinic-data/src/main/java/com/thefifthcontinent/sfgpetclinic/model/Visit.java
+++ sfg-pet-clinic-data/src/main/java/com/thefifthcontinent/sfgpetclinic/model/Visit.java
@@ -2,12 +2,26 @@ package com.thefifthcontinent.sfgpetclinic.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Visits")
 public class Visit extends BaseEntity {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Column(name = "time")
 	private LocalTime time;
+	
+	@Column(name = "description")
 	private String description;
+	
+	@ManyToOne
+	@JoinColumn(name = "pet_id")
 	private Pet pet;
 	
 	
