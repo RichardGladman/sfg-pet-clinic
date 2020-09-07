c9aba6aff598336a20572eb6553816f31406960c
Richard Gladman <gitlab@thefifthcontinent.com>
2020-09-07 16:18:44 +0100

    Visit service is a service!

--- sfg-pet-clinic-data/src/main/java/com/thefifthcontinent/sfgpetclinic/services/jpa/VisitServiceJPA.java
+++ sfg-pet-clinic-data/src/main/java/com/thefifthcontinent/sfgpetclinic/services/jpa/VisitServiceJPA.java
@@ -1,9 +1,14 @@
 package com.thefifthcontinent.sfgpetclinic.services.jpa;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
 import com.thefifthcontinent.sfgpetclinic.model.Visit;
 import com.thefifthcontinent.sfgpetclinic.repositories.VisitRepository;
 import com.thefifthcontinent.sfgpetclinic.services.VisitService;
 
+@Service
+@Profile("springdatajpa")
 public class VisitServiceJPA extends AbstractJPAService<Visit, VisitRepository> implements VisitService {
 
 	public VisitServiceJPA(VisitRepository repository) {
